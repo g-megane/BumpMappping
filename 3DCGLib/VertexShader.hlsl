2e1792f74ddd98ef46d2b2e8c3b4f9ecb4d36046
g-megane <pandoraxssscan.929@gmail.com>
2017-12-10 02:33:53 +0900

    不要部分の削除

--- 3DCGLib/VertexShader.hlsl
+++ 3DCGLib/VertexShader.hlsl
@@ -4,21 +4,18 @@ cbuffer ConstantBuffer : register(b0)
     matrix World;          // ワールド行列
     matrix View;           // ビュー行列
     matrix Projection;     // 射影行列
-    float4 vLightDir[2];   // ライトの座標
-    float4 vLightColor[2]; // ライトの色
-    float4 vOutputColor;   // ライト以外のモデルの色
 }
 
 struct VS_INPUT
 {
-    float4 Pos : POSITION; // 頂点位置
-    float3 Norm : NORMAL;  // 法線ベクトル
+    float4 Pos : POSITION;  // 頂点位置
+    float2 Tex : TEXCOORD0; // UV座標
 };
 
 struct PS_INPUT
 {
     float4 Pos : SV_POSITION;
-    float3 Norm : TEXCOORD0;
+    float2 Tex : TEXCOORD0;
 };
 
 // 頂点シェーダー
@@ -28,7 +25,7 @@ PS_INPUT VS(VS_INPUT input)
     output.Pos  = mul(input.Pos, World);
     output.Pos  = mul(output.Pos, View);
     output.Pos  = mul(output.Pos, Projection);
-    output.Norm = mul(float4(input.Norm, 0.0f), World).xyz;
+    output.Tex  = input.Tex;
 
     return output;
 }
